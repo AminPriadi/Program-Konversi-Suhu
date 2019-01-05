d1fd4a98649877f62f9f2b4946cd0d9160079f93
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 10:38:47 +0700

    Add files via upload

---
+++ Program mengkonversi suhu celcius ke farenheit dan reamur.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int celcius,farenheit,reamur;
+    cout<<"masukan suhu celcius =";
+    cin>>celcius;
+    cout<<"\n\t";
+     farenheit=9/5*celcius+32;
+     cout<<"suhu farenheitnya adalah =";
+     cout<<farenheit<<endl;
+
+     reamur=0.8*celcius;
+     cout<<"suhu reamurnya adalah =";
+     cout<<reamur;
+
+     return 0;
+}
